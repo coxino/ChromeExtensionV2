4af8e98379f9ad0e6add1669f608350d6dbe3c25
coxino <coxinotv@gmail.com>
2023-01-19 05:22:42 +0200

    getgame, show notification, display game in popup
    getgame, show notification, display game in popup

--- background.js
+++ background.js
@@ -1,52 +1,90 @@
-chrome.tabs.onUpdated.addListener(function(info) {
-    chrome.tabs.query({}).then(logTabs, onError);
-});
-
-var currentGameUrl = "";
+var token = "";
+var currentGameUrl = "";   
 
 //TODO:Add all websites
 const CasinoSites = [    
     "superbet.ro/joc/",
     "mrbit.ro/en/games/",
-    "betano.com/casino/games/"]
-    
-    function logTabs(tabs) {
-        for (const tab of tabs) {   
-            if(CasinoSites.some(v => tab.url.includes(v))){   
-                //Avoid double change   
-                if(tab.url != currentGameUrl){
-                    sendRequest(tab.url);
-                    currentGameUrl = tab.url;
-                    console.log("CurrentGameUrl changed:" + currentGameUrl);
-                }
+    "betano.com/casino/games/"
+]
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    token = message;
+    console.log("Recieved token " + token);
+});
+
+chrome.tabs.onUpdated.addListener(function(info) {  
+    console.log("" + info)   
+    if(token != null){
+        chrome.tabs.query({}).then(logTabs, onError);
+    }
+});
+
+function logTabs(tabs) { 
+    //TODO: ADD ChEcKS FOR MULTIPLE WEBSITES      
+    for (const tab of tabs) {   
+        if(CasinoSites.some(v => tab.url.includes(v))){   
+            //Avoid double change to minimize data exchange
+            if(tab.url != currentGameUrl){
+                sendRequest(tab.url);
+                currentGameUrl = tab.url;                
             }
         }
     }
-    
-    function onError(error) {
-        console.error(`Error: ${error}`);
-    }
-    
-    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImNveGlubyAgICAiLCJQYXNzd29yZCI6IlczeU12SHlUNzQ1YVpuYUMiLCJVc2VySWQiOiI3OTYzZmYwOC04OGU2LTRjZTUtOGI0Zi1mN2MwYmNiOWU3ODMiLCJuYmYiOjE2NzM5NjY1NzYsImV4cCI6MTY3NDU3MTM3NiwiaWF0IjoxNjczOTY2NTc2LCJpc3MiOiJodHRwOi8vbXlzaXRlLmNvbSIsImF1ZCI6Imh0dHA6Ly9teWF1ZGllbmNlLmNvbSJ9.iYiFTFpeYmEinDL3pGjiNuojMzXCtFaMfUiuJCXQljk";
-    
-    function sendRequest(url){  
-        fetch("https://coxino.go.ro:5000/api/player/login?token=" + token + "&url=" + url)
-        .then(
-            function(response) {
-                if (response.status !== 200) {
-                    console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-                    return;
-                }
-                
-                response.json().then(function(data) {
-                    console.log(data);
-                });
-            }
-            )        
-            .catch(function(err) {
-                console.log('Fetch Error :-S', err);
-            });   
+}
+
+function onError(error) {
+    //TODO:add log to server
+    console.error(`Error: ${error}`);
+}
+
+function sendRequest(url){  
+    fetch("https://coxino.go.ro:5000/api/extension/setinplay?token=" + token + "&url=" + url, {
+    //mode: 'no-cors',
+    method: 'GET',
+    headers: { Accept: 'application/json'}})
+    .then(response => { 
+        if (response.status !== 200) {              
+            console.log("response" +response);     
+            chrome.storage.local.set({ "serverStatus": "off" }, function(){});
+            notify("Erroare","./images/icon.png");
+            return;
         }
         
-        
+        response.json().then(function(data) {
+            chrome.storage.local.set({ "serverStatus": "on" }, function(){});
+            console.log(JSON.stringify(data));
+            notify(data.name +" by "+ data.provider,"./images/icon.png");
+            chrome.storage.local.set({ "currentGameName": data.name,
+            "currentGameIcon": data.image,
+            "currentGameRecord": getRecord(data.rounds),
+            "currentGameProvider": data.provider },function(){});
+        });
+    })
+    .catch(function(err) {
+        chrome.storage.local.set({ "serverStatus": "off" }, function(){});
+        notify("Server Off","./images/icon.png");
+    });
+}
+
+var i = 0;        
+function notify(_message,_icon){   
+    var opt = {
+        type: 'list',
+        title: 'CasinoBroadcastHelper',
+        message: 'New game Detected',
+        priority: 1,
+        items: [{ title: 'New Game: ', message: _message}],
+        iconUrl: _icon
+    };
+    chrome.notifications.create(i++ + "", opt, function(id) {});
+}
+
+
+function getRecord(rounds)
+{
+    var best_pay = rounds.map(x=>x.payAmount).reduce((a,b)=>Math.max(a,b));
+    var best_x = rounds.map(x=>x.multiplier).reduce((a,b)=>Math.max(a,b));
+
+    return Intl.NumberFormat('en-US').format(best_pay);
+}
--- popup.js
+++ popup.js
@@ -1,27 +1,78 @@
-var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VybmFtZSI6ImNveGlubyAgICAiLCJQYXNzd29yZCI6IlczeU12SHlUNzQ1YVpuYUMiLCJVc2VySWQiOiI3OTYzZmYwOC04OGU2LTRjZTUtOGI0Zi1mN2MwYmNiOWU3ODMiLCJuYmYiOjE2NzM5NjY1NzYsImV4cCI6MTY3NDU3MTM3NiwiaWF0IjoxNjczOTY2NTc2LCJpc3MiOiJodHRwOi8vbXlzaXRlLmNvbSIsImF1ZCI6Imh0dHA6Ly9teWF1ZGllbmNlLmNvbSJ9.iYiFTFpeYmEinDL3pGjiNuojMzXCtFaMfUiuJCXQljk";
 
-testConnection();
+getServerStatus();
+refreshGame();
 
-function testConnection(){  
-  const req = new XMLHttpRequest();
-  const baseUrl = "https://coxino.go.ro:5000/api/player/loginstatus/" + token;   
+function getServerStatus()
+{ 
+    chrome.storage.sync.get(["token"], function(syncItems){     
+        var token = syncItems["token"];       
+        if(token != null && token.length > 0){  
+            sendToken(token);
+            document.getElementById("login-box").classList.add("hidden");       
+            chrome.storage.local.get(["serverStatus"], function(localItems){  
+                if(localItems["serverStatus"] == "on"){        
+                    document.getElementById("message").innerHTML = "Active";
+                    document.getElementById("message").classList.remove("hold");
+                    document.getElementById("message").classList.remove("error");
+                    document.getElementById("message").classList.add("success");
+                }
+                else if(localItems["serverStatus"] == "off"){      
+                    document.getElementById("message").innerHTML = "Disconected";
+                    document.getElementById("message").classList.remove("hold");
+                    document.getElementById("message").classList.remove("success");
+                    document.getElementById("message").classList.add("error");
+                }
+                else{
+                    document.getElementById("message").innerHTML = "Waiting Casino Page";        
+                    document.getElementById("message").classList.remove("error");
+                    document.getElementById("message").classList.remove("success");
+                    document.getElementById("message").classList.add("hold");
+                }
+            });
+        }
+        else{
+            document.getElementById("update-box").classList.add("hidden");
+        }
+    });
+}
 
-  req.open("GET", baseUrl, true);
-  req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  req.send();
+document.getElementById("save-btn").addEventListener('click',(event)=>{
+    setServerAccessToken();
+})
 
-  req.onreadystatechange = function() { // Call a function when the state changes.
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-          console.log("Got response 200!" + this.response);
-          document.getElementById("green-dot").classList.remove("hidden");
-          document.getElementById("message").innerHTML = "Active";
-          document.getElementById("message").classList.remove("error");
-          document.getElementById("message").classList.add("success");
-      }else{
-          document.getElementById("green-dot").classList.add("hidden");
-          document.getElementById("message").innerHTML = "Disconected";
-          document.getElementById("message").classList.remove("success");
-          document.getElementById("message").classList.add("error");
-      }
-  }
+function setServerAccessToken(){
+    var token = document.getElementById("token").value;
+    document.getElementById("login-box").classList.add("hidden");
+    
+    chrome.storage.sync.set({ "token": token }, function(){
+        sendNotification("Token Saved", token);
+    });
+}
+
+function sendNotification(_title, _message){
+    id = "CasinoBroadcastHelper";
+    var opt = {
+        type: 'list',
+        title: 'CasinoBroadcastHelper',
+        message: '',
+        priority: 1,
+        items: [{ title: _title, message: _message}],
+        iconUrl: "./images/icon.png"
+    };
+    chrome.notifications.create(id, opt, function(id) {});
+}
+
+function sendToken(token){
+    chrome.runtime.sendMessage(token, (response) => { 
+        console.log("Sent Token")
+    });
+}
+
+function refreshGame(){
+    chrome.storage.local.get(["currentGameName","currentGameIcon","currentGameProvider","currentGameRecord"],localStorage=>{
+        document.getElementById("currentGameName").innerText = localStorage["currentGameName"];
+        document.getElementById("currentGameIcon").src = localStorage["currentGameIcon"];
+        document.getElementById("currentGameProvider").innerText = localStorage["currentGameProvider"];
+        document.getElementById("currentGameRecord").innerText = localStorage["currentGameRecord"];
+    });
 }
