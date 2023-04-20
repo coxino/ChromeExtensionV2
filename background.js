9876fc4946d9021eb92af3d045f1cb7986d8eb97
Bratianu Cosmin <coxinotv@gmail.com>
2023-04-20 17:36:35 +0300

    updated
    updated

--- background.js
+++ background.js
@@ -1,92 +1,246 @@
-var token = "";
-var currentGameUrl = "";   
+var token = null;
+var currentGameUrl = "";
+var isSet = false;
+
 
 //TODO:Add all websites
-const CasinoSites = [    
-    "superbet.ro/joc/",
-    "mrbit.ro/en/games/",
-    "betano.com/casino/games/"
+const CasinoSites = [
+    //
+    { site: "superbet.ro/joc/", class: "mycasino-page-title", type: 'super', no: 0 },
+    
+    //URLS
+    { site: "mrbit.ro/en/games/", class: "", type: 'url', no: 0 },
+    { site: "conticazino.ro/casino/", class: "", type: 'url', no: 0 },
+    { site: "online.getsbet.ro/casino/game/", class: "", type: 'url', no: 0 },
+    { site: "winboss.ro/en/casino-games/slots/", class: "", type: 'url', no: 0 },
+    { site: "www.cashpot.ro/games", class: "", type: 'url', no: 0 },
+    //https://winboss.ro/ro/casino-games
+    //https://winboss.ro/ro/casino-games/slots/
+    //CLASSES
+    { site: "betano.com/casino/games/", class: "game-title", type: 'class', no: 0 },
 ]
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    token = message;
+    if (message != null && message.length > 0)
+        token = message;
     console.log("Recieved token " + token);
+    sendResponse('done');
+    chrome.tabs.query({ active: true, currentWindow: true }).then(logTabs, onError);
 });
 
-chrome.tabs.onUpdated.addListener(function(info) {  
-    console.log("" + info)   
-    if(token != null){
-        chrome.tabs.query({}).then(logTabs, onError);
-    }
+chrome.tabs.onUpdated.addListener(function (info) {
+    console.log("TabChanged" + JSON.stringify(info));
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }).then(logTabs, onError);
 });
 
-function logTabs(tabs) { 
-    //TODO: ADD ChEcKS FOR MULTIPLE WEBSITES      
-    for (const tab of tabs) {   
-        if(CasinoSites.some(v => tab.url.includes(v))){   
+const _delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function logTabs(tabs) {
+    if (token == null)
+        chrome.storage.sync.get(["token"], function (syncItems) {
+            token = syncItems["token"];
+            console.log("GOTTOKEN" + token);
+        });
+
+    for (const tab of tabs) {
+        if (CasinoSites.map(x => x.site).some(v => tab.url.includes(v))) {
             //Avoid double change to minimize data exchange
-            if(tab.url != currentGameUrl){
-                sendRequest(tab.url);
-                currentGameUrl = tab.url;                
+            if (tab.url != currentGameUrl) {
+                currentGameUrl = tab.url;
+                var classType = CasinoSites.find(x => tab.url.includes(x.site)).type;
+                var identifier = CasinoSites.find(x => tab.url.includes(x.site)).class;
+                var identifierNo = CasinoSites.find(x => tab.url.includes(x.site)).no;
+                switch (classType) {
+                    case 'super':
+                        chrome.scripting.executeScript({
+                            target: { tabId: tab.id },
+                            args: [identifier],
+                            function: extractGameFromSuperbet,
+                        });
+                        break;
+                    case 'class':
+                        chrome.scripting.executeScript({
+                            target: { tabId: tab.id },
+                            args: [identifier, identifierNo],
+                            function: extractGameFromClass,
+                        });
+                        break;
+
+                    case 'tag':
+                        console.log("TAG" + identifier);
+                        chrome.scripting.executeScript({
+                            target: { tabId: tab.id },
+                            args: [identifier,identifierNo],
+                            function: extractGameFromTag,
+                        });
+                        break;
+
+                    case 'url':
+                        console.log("TAG" + identifier);
+                        await _delay(1000);
+                        sendCurrentGameRequest(tab.url);
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
 }
+
+
+//id
+function extractGameFromId(divId) {
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const yourFunction = async () => {
+        await delay(1000);
+        console.log("Waited 1s");
+        var gameName = document.getElementById(divId)[0].innerHTML;
+        console.log("game: " + gameName);
+        chrome.runtime.sendMessage({ type: "sendGame", gameNume: gameName }, (response) => {
+            console.log("Sent game");
+            return;
+        });
+    };
+
+    yourFunction();
+}
+
+
+///TAG
+function extractGameFromTag(tagName,tagno) {
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const yourFunction = async () => {
+        await delay(2000);
+        console.log("Waited 1s");
+        var gameName = document.getElementsByTagName(tagName)[tagno].innerHTML;
+        console.log("game: " + gameName);
+        chrome.runtime.sendMessage({ type: "sendGame", gameNume: gameName }, (response) => {
+            console.log("Sent game");
+            return;
+        });
+    };
+
+    yourFunction();
+}
+
+///CLASS
+function extractGameFromClass(className, no) {
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const yourFunction = async () => {
+        await delay(1000);
+        console.log("Waited 1s");
+
+        //only superbet
+        //document.getElementsByClassName('left-sidebar')[0].style.display = 'none';
+        var gameName = document.getElementsByClassName(className)[no].innerHTML;
+        console.log("game: " + gameName);
+        chrome.runtime.sendMessage({ type: "sendGame", gameNume: gameName }, (response) => {
+            console.log("Sent game");
+            return;
+        });
+    };
+
+    yourFunction();
+}
+
+function extractGameFromSuperbet(className) {
+
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+
+    const yourFunction = async () => {
+        await delay(1000);
+
+        console.log("Waited 1s");
+
+        //only superbet
+        document.getElementsByClassName('left-sidebar')[0].style.display = 'none';
+        var gameName = document.getElementsByClassName(className)[0].innerHTML;
+        chrome.runtime.sendMessage({ type: "sendGame", gameNume: gameName }, (response) => {
+            console.log("Sent game" + gameName);
+            return;
+        });
+    };
+
+    yourFunction();
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type == "sendGame")
+        sendCurrentGameRequest(request.gameNume);
+    sendResponse();
+});
 
 function onError(error) {
     //TODO:add log to server
     console.error(`Error: ${error}`);
 }
 
-function sendRequest(url){  
-    fetch("https://coxino.go.ro:5000/api/extension/setinplay?token=" + token + "&url=" + url, {
-    //mode: 'no-cors',
-    method: 'GET',
-    headers: { Accept: 'application/json'}})
-    .then(response => { 
-        if (response.status !== 200) {              
-            console.log("response" +response);     
-            chrome.storage.local.set({ "serverStatus": "off" }, function(){});
-            notify("Erroare","./images/icon.png");
-            return;
-        }
-        
-        response.json().then(function(data) {
-            chrome.storage.local.set({ "serverStatus": "on" }, function(){});
-            console.log(JSON.stringify(data));
-            notify(data.name +" by "+ data.provider,"./images/icon.png");
-            chrome.storage.local.set({ "currentGameName": data.name,
-            "currentGameIcon": data.image,
-            "currentGameRecord": getRecord(data.rounds),
-            "currentGameProvider": data.provider },function(){});
-        });
+
+
+function sendCurrentGameRequest(url) {
+    fetch("https://coverlay20230404202215.azurewebsites.net/api/extension/setinplay?token=" + token + "&url=" + url, {
+        method: 'GET',
     })
-    .catch(function(err) {
-        chrome.storage.local.set({ "serverStatus": "off" }, function(){});
-        notify("Server Off","./images/icon.png");
-    });
+        .then(response => {
+            if (response.status !== 200) {
+
+                chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+                notify("Erroare", "./images/icon.png");
+                return;
+            }
+            else {
+                response.json().then(function (data) {
+                    try {
+                        chrome.storage.local.set({ "serverStatus": "on" }, function () { });
+                        console.log(JSON.stringify(data));
+                        notify(data.name + " by " + data.provider, "./images/icon.png");
+                        chrome.storage.local.set({
+                            "currentGameName": data.name,
+                            "currentGameIcon": data.image,
+                            "currentGameRecord": getRecord(data.rounds),
+                            "currentGameProvider": data.provider
+                        }, function () { });
+                    }
+                    catch (e) { }
+                });
+            }
+        })
+        .catch(function (err) {
+            chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+            notify("Server Off", "./images/icon.png");
+        });
 }
 
-var i = 0;        
-function notify(_message,_icon){   
+var i = 0;
+function notify(_message, _icon) {
     var opt = {
         type: 'list',
         title: 'CasinoBroadcastHelper',
         message: 'New game Detected',
         priority: 1,
-        items: [{ title: 'New Game: ', message: _message}],
+        items: [{ title: 'New Game: ', message: _message }],
         iconUrl: _icon
     };
-    chrome.notifications.create(i++ + "", opt, function(id) {});
+    chrome.notifications.create(i++ + "", opt, function (id) { });
 }
 
 
-function getRecord(rounds)
-{
-    if(rounds.length > 0){
-        var best_pay = rounds?.map(x=>x.payAmount)?.reduce((a,b)=>Math.max(a,b,0)) ?? 0;
-        var best_x = rounds?.map(x=>x.multiplier)?.reduce((a,b)=>Math.max(a,b,0)) ?? 0;
+function getRecord(rounds) {
+    if (rounds.length > 0) {
+        var best_pay = rounds?.map(x => x.payAmount)?.reduce((a, b) => Math.max(a, b, 0)) ?? 0;
+        var best_x = rounds?.map(x => x.multiplier)?.reduce((a, b) => Math.max(a, b, 0)) ?? 0;
     }
-    
+
     return Intl.NumberFormat('en-US').format(best_pay ?? 0);
 }
--- popup.js
+++ popup.js
@@ -1,72 +1,96 @@
 
 getServerStatus();
-refreshGame();
 
-function getServerStatus()
-{ 
-    chrome.storage.sync.get(["token"], function(syncItems){     
-        var token = syncItems["token"];       
-        if(token != null && token.length > 0){  
+var _token = "";
+
+let Games;
+
+loadGames();
+var AllGamesAuto = document.getElementById('all-games');
+
+
+
+function getServerStatus() {
+    chrome.storage.sync.get(["token"], function (syncItems) {
+        var token = syncItems["token"];
+        var isLoggedIn = false;
+        if (token != null && token.length > 0) {
+            isLoggedIn = true;
             sendToken(token);
-            document.getElementById("login-box").classList.add("hidden");       
-            chrome.storage.local.get(["serverStatus"], function(localItems){  
-                if(localItems["serverStatus"] == "on"){        
+            _token = token;
+            document.getElementById("tab-changer").classList.remove("hidden");
+            document.getElementById("login-box").classList.add("hidden");
+            chrome.storage.local.get(["serverStatus"], function (localItems) {
+                if (localItems["serverStatus"] == "on") {
                     document.getElementById("message").innerHTML = "Active";
                     document.getElementById("message").classList.remove("hold");
                     document.getElementById("message").classList.remove("error");
                     document.getElementById("message").classList.add("success");
+
+                    document.getElementById("dot").classList.add("green");
                 }
-                else if(localItems["serverStatus"] == "off"){      
+                else if (localItems["serverStatus"] == "off") {
                     document.getElementById("message").innerHTML = "Disconected";
                     document.getElementById("message").classList.remove("hold");
                     document.getElementById("message").classList.remove("success");
                     document.getElementById("message").classList.add("error");
+
+                    document.getElementById("dot").classList.add("red");
                 }
-                else{
-                    document.getElementById("message").innerHTML = "Waiting Casino Page";        
+                else {
+                    document.getElementById("message").innerHTML = "Waiting Casino Page";
                     document.getElementById("message").classList.remove("error");
                     document.getElementById("message").classList.remove("success");
                     document.getElementById("message").classList.add("hold");
+
+                    document.getElementById("dot").classList.add("hidden");
                 }
             });
         }
-        else{
+        else {
+            document.getElementById("login-box").classList.remove("hidden");
             document.getElementById("update-box").classList.add("hidden");
+            document.getElementById("tab-changer").classList.add("hidden");
         }
+
+        refreshGame(isLoggedIn);
     });
 }
 
-function setServerAccessToken(){
+function setServerAccessToken() {
     var token = document.getElementById("token").value;
     document.getElementById("login-box").classList.add("hidden");
-    
-    chrome.storage.sync.set({ "token": token }, function(){
+    document.getElementById("update-box").classList.remove("hidden");
+    document.getElementById("tab-changer").classList.remove("hidden");
+    chrome.storage.sync.set({ "token": token }, function () {
         sendNotification("Token Saved", token);
     });
 }
 
-function sendNotification(_title, _message){
-    id = "CasinoBroadcastHelper";
+function sendNotification(_title, _message) {
+    var id = "CasinoBroadcastHelper";
     var opt = {
         type: 'list',
         title: 'CasinoBroadcastHelper',
         message: '',
         priority: 1,
-        items: [{ title: _title, message: _message}],
+        items: [{ title: _title, message: _message }],
         iconUrl: "./images/icon.png"
     };
-    chrome.notifications.create(id, opt, function(id) {});
+    chrome.notifications.create(id, opt, function (id) { });
 }
 
-function sendToken(token){
-    chrome.runtime.sendMessage(token, (response) => { 
+function sendToken(token) {
+    chrome.runtime.sendMessage(token, (response) => {
         console.log("Sent Token");
         return;
     });
 }
 
-function refreshGame(){
-    chrome.storage.local.get(["currentGameName","currentGameIcon","currentGameProvider","currentGameRecord"],localStorage=>{
+function refreshGame(isLoggedIn) {
+    if (!isLoggedIn)
+        return;
+    chrome.storage.local.get(["currentGameName", "currentGameIcon", "currentGameProvider", "currentGameRecord"], localStorage => {
         document.getElementById("currentGameName").innerText = localStorage["currentGameName"];
         document.getElementById("currentGameIcon").src = localStorage["currentGameIcon"];
         document.getElementById("currentGameProvider").innerText = localStorage["currentGameProvider"];
@@ -88,16 +112,200 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+function sendNewRecordForCurrentGame() {
+    var recordAmmonut = document.getElementById("currGameRecord").value;
+    var recordBet = document.getElementById("currRecordBet").value;
+    chrome.storage.sync.get(["token"], function (syncItems) {
+        var token = syncItems["token"];
+        fetch(`https://coverlay20230404202215.azurewebsites.net/api/extension/setrecord?&record=${recordAmmonut}&bet=${recordBet}`, {
+            method: 'GET',
+            headers: {
+                Token: token,
+                Accept: 'application/json',
+                'Access-Control-Allow-Origin': "*"
+            }
+        })
+            .then(response => {
+                response.header("Access-Control-Allow-Origin", "*")
+                if (response.status !== 200) {
+                    document.getElementById("recordError").innerText = "Error In Saving Record";
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    document.getElementById("recordError").innerText = data;
+                });
+            })
+            .catch(function (err) {
+                chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+                notify("Server Off", "./images/icon.png");
+            });
+    });
+}
+
+
+function sendCurrentGameToBonusHunt() {
+    var bet = document.getElementById("currBet").value;
+    chrome.storage.sync.get(["token"], function (syncItems) {
+        var token = syncItems["token"];
+        fetch("https://coverlay20230404202215.azurewebsites.net/api/extension/currentgametobh?bet=" + bet, {
+            method: 'GET',
+            headers: {
+                Token: token,
+                Accept: 'application/json',
+                'Access-Control-Allow-Origin': "*"
+            }
+        })
+            .then(response => {
+                response.header("Access-Control-Allow-Origin", "*")
+                if (response.status !== 200) {
+                    document.getElementById("recordError").innerText = "Error In Saving Record";
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    document.getElementById("recordError").innerText = data;
+                });
+            })
+            .catch(function (err) {
+                chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+                notify("Server Off", "./images/icon.png");
+            });
+    });
+}
+
+document.getElementById("currBet").addEventListener('input', (event) => {
+    var obj = document.getElementById("currBet");
+    obj.value = formatNumber(obj.value);
+})
+
+document.getElementById("currGameRecord").addEventListener('input', (event) => {
+    var obj = document.getElementById("currGameRecord");
+    obj.value = formatNumber(obj.value);
+})
+
+function formatNumber(num) {
+    let str = num.toString().replace(/[^0-9.]/g, ''); // remove all non-numeric characters (including spaces)
+    let result = '';
+    let decimalPart = '';
+    if (str.includes('.')) {
+        decimalPart = str.substr(str.indexOf('.'));
+        str = str.substr(0, str.indexOf('.'));
+    }
+    for (let i = 0; i < str.length; i++) {
+        result += str[i];
+        if ((str.length - i - 1) % 3 === 0 && i !== str.length - 1) {
+            result += ',';
+        }
+    }
+    return result + decimalPart;
+}
+
+document.getElementById("manualgamesave").addEventListener('click', (event) => {
+    var game = document.getElementById("manualgamename").value;
+    fetch("https://coverlay20230404202215.azurewebsites.net/api/extension/setinplay?token=" + _token + "&url=" + game, {        
+        method: 'GET',
+        headers: { 
+            Accept: 'application/json',
+            'Access-Control-Allow-Origin': "*"
+         }
+    })
+        .then(response => {
+            response.header("Access-Control-Allow-Origin", "*")
+            if (response.status !== 200) {
+                console.log("response" + response);
+                chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+                notify("Erroare", "./images/icon.png");
+                return;
+            }
+
+            response.json().then(function (data) {
+                chrome.storage.local.set({ "serverStatus": "on" }, function () { });
+                chrome.storage.local.set({
+                    "currentGameName": data.name,
+                    "currentGameIcon": data.image,
+                    "currentGameRecord": getRecord(data.rounds),
+                    "currentGameProvider": data.provider
+                }, function () { });
+                refreshGame(true);
+                sendNotification(data.name, data.name + " by " + data.provider)
+            });
+        })
+        .catch(function (err) {
+            chrome.storage.local.set({ "serverStatus": "off" }, function () { });
+            notify("Server Off", "./images/icon.png");
+        });
+})
+
+document.getElementById("recordsave").addEventListener('click', (event) => {
+    sendNewRecordForCurrentGame();
+})
+
+document.getElementById("bhsave").addEventListener('click', (event) => {
+    sendCurrentGameToBonusHunt();
+})
+
 //EVENTS
 
-document.getElementById("save-btn").addEventListener('click',(event)=>{
+document.getElementById("save-btn").addEventListener('click', (event) => {
     setServerAccessToken();
 })
 
-document.getElementById("gamerecord-button").addEventListener('click',(event)=>{
-     openCity(event, 'gamerecord')
+document.getElementById("gamerecord-button").addEventListener('click', (event) => {
+    openCity(event, 'gamerecord')
 })
 
-document.getElementById("bonushunt-button").addEventListener('click',(event)=>{
+document.getElementById("bonushunt-button").addEventListener('click', (event) => {
     openCity(event, 'bonushunt')
 })
+
+document.getElementById("manualgame-button").addEventListener('click', (event) => {
+    openCity(event, 'manualgame')
+})
+
+function loadGames() {
+    fetch("https://coxino.ro/assets/database/AllGames.json", {
+        mode: 'no-cors',
+        method: 'GET',
+        headers: { Accept: 'application/json' }
+    }).then(response => {
+        if (response.status !== 200) {
+            console.log("response" + JSON.stringify(response));
+        }
+        else {
+            response.header("Access-Control-Allow-Origin", "*")
+            response.json().then(function (data) {
+                console.log(data);
+                Games = data.map(x => '<option>' + x.Name + '</option>');
+                AllGamesAuto.innerHTML += Games;
+            });
+        }
+    })
+        .catch(function (err) {
+            console.log("Games Error" + err);
+        });
+    //https://coxino.ro/assets/database/AllGames.json
+    //TODO: FETCH + AUTOCOMPLETE
+}
+
+function getRecord(rounds) {
+    if (rounds.length > 0) {
+        var best_pay = rounds?.map(x => x.payAmount)?.reduce((a, b) => Math.max(a, b, 0)) ?? 0;
+        var best_x = rounds?.map(x => x.multiplier)?.reduce((a, b) => Math.max(a, b, 0)) ?? 0;
+    }
+
+    return Intl.NumberFormat('en-US').format(best_pay ?? 0);
+}
+
+var i = 0;
+function notify(_message, _icon) {
+    var opt = {
+        type: 'list',
+        title: 'CasinoBroadcastHelper',
+        message: 'New game Detected',
+        priority: 1,
+        items: [{ title: 'New Game: ', message: _message }],
+        iconUrl: _icon
+    };
+    chrome.notifications.create(i++ + "", opt, function (id) { });
+}
